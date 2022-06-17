fe5a9b1fd9580dfbc79fd94ff04390f9f5ce8c7f
Sean Oh <ohsean812@gmail.com>
2022-06-16 20:00:49 -0400

    edit styles, add more event

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const plane = document.getElementById("plane");
+
+function movePlaneLeft() {
+  const leftNumbers = plane.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 20) {
+    plane.style.left = `${left - 20}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    movePlaneLeft();
+  }
+});
+
+function movePlaneRight() {
+    const leftNumbers = plane.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 1090) {
+      plane.style.left = `${left + 20}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      movePlaneRight();
+    }
+  });
+
+const input = document.getElementById('plane');
+
+function clickAlert() {
+  alert('"Wherever you go, go with all your heart" - Confucius');
+  alert('Thanks for visiting my website.  Have a nice day!');
+}
+
+input.addEventListener('click', clickAlert);
